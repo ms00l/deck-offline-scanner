b84028689d32b973de6de8fe575768e5892c64f2
ms00l <moesuleiman8@gmail.com>
2026-01-16 15:18:17 -0500

    added parsing

--- scanner.py
+++ scanner.py
@@ -14,7 +14,7 @@ def find_steam_root() -> Path:
     # TODO: loop through candidates and return the first that is a directory
     # Use: path.is_dir()
 
-    print("DEBUG CHECK: checking Steam root candidates...")
+    print("Checking Steam root candidates...")
     for c in candidates:
         if c.is_dir():
             return c
@@ -39,6 +39,9 @@ def read_text(path) -> str:
     return path.read_text(encoding="utf-8", errors="ignore")
 
 def extract_quoted_value(text, key) -> str | None:
+    """
+    This function splits appmanifest text, uses [3] index to grab value of key
+    """
     splitText = text.splitlines()
 
     for t in splitText:
@@ -52,35 +55,56 @@ def extract_quoted_value(text, key) -> str | None:
 
     return None
 
+def parse_manifest(manifest_path) -> tuple[str, str]:
+    """
+    This function is a full open of manifest, grab (appid, name) and return values one all
+    """
+    text = read_text(manifest_path)
+
+    parsed_appid = extract_quoted_value(text, "appid")
+    parsed_name = extract_quoted_value(text, "name")
+
+    parsed_info = parsed_appid, parsed_name
+
+    if any(item is None for item in parsed_info):
+        raise RuntimeError(f"Parse failed for {manifest_path}!")
+
+    return parsed_info
 
 def main() -> None:
     steam_root = find_steam_root()
-    print(f"Steam root: {steam_root}")
+    # print(f"Steam root: {steam_root}")
 
     steamapps = steam_root / "steamapps"
-    library_vdf = steamapps / "libraryfolders.vdf"
 
-    print(f"Steamapps: {steamapps}")
-    print(f"Library file: {library_vdf}")
-    print(f"Exists (answer as boolean): {library_vdf.is_file()}")
-
+    """
+    ####### this was for debug
+    # library_vdf = steamapps / "libraryfolders.vdf"
+    # print(f"Steamapps: {steamapps}")
+    # print(f"Library file: {library_vdf}")
+    # print(f"Exists (answer as boolean): {library_vdf.is_file()}")
+    """
     steam_appmanifest = get_appmanifests(steamapps)
     numOf_steam_appmanifest = len(steam_appmanifest)
+
     print(f"Found {numOf_steam_appmanifest} appmanifests.")
-    print("These are the first 5 of them: ")
-    for path in steam_appmanifest[:5]:
-        print(f"{path}")
-    debug_input = input("Proceed with showing the first manifest? [y/n]: ")
-    if debug_input == "y":
-        first_manifest = steam_appmanifest[0]
-        text = read_text(first_manifest)
-        print("\n".join(text.splitlines()[:10]))
-        debug_input = input("Proceed with extraction of values from manifest? [y/n]: ")
-        if debug_input == "y":
-            print(extract_quoted_value(text, "appid"))
-            print(extract_quoted_value(text, "name"))
-        else: pass
-    else: pass
+    for path in steam_appmanifest:
+        print(parse_manifest(path))
+
+    """
+    ####### this was for debug
+    # debug_input = input("Proceed with showing the first manifest? [y/n]: ")
+    # if debug_input == "y":
+    #     text = read_text(first_manifest)
+    #     print("\n".join(text.splitlines()[:10]))
+    #     debug_input = input("Proceed with extraction of values from manifest? [y/n]: ")
+    #     if debug_input == "y":
+    #         print(extract_quoted_value(text, "appid"))
+    #         print(extract_quoted_value(text, "name"))
+    #     else: pass
+    # elif debug_input == "n":
+    #     print("Moving on to parsing the first manifest in full...")
+    """
 
 if __name__ == "__main__":
     main()
